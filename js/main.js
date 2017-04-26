8a85bcb1995b9b2eca15b25f42097f06fb9e0981
Estevam Gomes <estevamgomes@gmail.com>
2017-04-26 18:09:29 -0300

    Interactive architecture [work in progress]
    Website about sensors and actuators related with my master’s thesis
    about interactive architecture.

---
+++ js/main.js
@@ -0,0 +1,178 @@
+var ajaxOpen = false;
+
+/////////////////////////
+//// DRAG AND SCROLL ////
+/////////////////////////
+// fonte: http://stackoverflow.com/questions/4744261/how-to-scroll-a-webpage-by-clicking-and-dragging-on-the-body-like-touch-device
+
+/*
+var curYPos = 0,
+	curXPos = 0,
+	curDown = false;
+
+$(document).on("mousemove", function (event) {
+	if (curDown === true) {
+		$(document).scrollTop(parseInt($(document).scrollTop() + (curYPos - event.pageY)));
+		$(document).scrollLeft(parseInt($(document).scrollLeft() + (curXPos - event.pageX)));
+	}
+}).on("mousedown", function (e) {
+	if(!ajaxOpen) {
+		e.preventDefault();
+		curDown = true;
+		curYPos = e.pageY;
+		curXPos = e.pageX;
+	}
+});
+
+$(window).on("mouseup", function (e) {
+	curDown = false;
+});
+
+// Stop dragging if mouse leaves the window (Not essential, can be removed without negative effects)
+$(window).on("mouseout", function (e) {
+	curDown = false;
+});
+*/
+//// FIM DRAG AND SCROLL ////
+
+
+Math.map = function(x, in_min, in_max, out_min, out_max) {
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
+Math.constrain = function(x, min, max) {
+	return ((x) < (min) ? (min) : ((x) > (max) ? (max) : (x)));
+}
+
+
+$(document).ready(function(){
+
+	// ljs.initHighlightingOnLoad(); // Highlight code
+	$('pre code').each(function(i, block) {
+		hljs.highlightBlock(block);
+	});
+
+	/////////////////////
+	//// MENU TOGGLE ////
+	/////////////////////
+
+	$(document).scroll(function() {
+		var scrollTop = $(document).scrollTop();
+		var scrollTopMin = $('#nav-main').offset().top + $('#nav-main').height();
+		if(scrollTop > scrollTopMin) {
+			$('#nav-main .toggle').addClass('show');
+		} else {
+			$('#nav-main .toggle').removeClass('show');
+		}
+	});
+
+
+	//////////////////////
+	//// SERVO SLIDER ////
+	//////////////////////
+
+	$('#servo-animation-counter').html('0°');
+	$('#servo-slider-counter').html('0');
+
+    $('#servo-slider').slider({
+		value: 0,
+		min: 0,
+		max: 1023,
+		step: 1,
+		slide: function( event, ui ) {
+
+			var step = Math.round(Math.map(ui.value, 0, 1023, 19, 1));
+			var newYPos = step == 19 ? -120 : step * (-120);
+			$('#servo-animation .servo-horn').css('backgroundPosition', '0 ' + newYPos + 'px');
+
+			step = Math.round(Math.map(ui.value, 0, 1023, 1, 19));
+			$('#servo-animation-counter').html(((step - 1) * 10) + '°');
+			$('#servo-slider-counter').html(ui.value);
+		}
+  	});
+
+
+	////////////////////////////
+	//// VIDEO PROGRESS BAR ////
+	////////////////////////////
+
+	$('#matriz video').parent().each(function(){
+		$(this).append('<div class="progress-wrapper"><div class="progress-bar"></div></div>');
+
+		var $video = $(this).find('video');
+		var $progressbar = $(this).find('.progress-bar');
+
+		$video.bind('timeupdate', videoTimeUpdateHandler);
+
+		function videoTimeUpdateHandler(e) {
+			var video = $video.get(0);
+			$progressbar.width(Math.floor(100 * video.currentTime / video.duration) + '%');
+		}
+	});
+
+
+	///////////////////////////////////////////
+	//// HIGHLIGHT TABLE HEADER PLAY VIDEO ////
+	///////////////////////////////////////////
+	
+	// $('video').each(function(){$(this).get(0).play();});
+
+	$('#matriz td video').parent()
+		.mouseenter(function(event){
+			$(this).parent().find('th').addClass('highlight');
+			$(this).parent().parent().find('tr:first th').eq($(this).index()).addClass('highlight');
+			$(this).find('video').get(0).play();
+		})
+		.mouseleave(function(event){
+			$(this).parent().find('th').removeClass('highlight');
+			$(this).parent().parent().find('tr:first th').eq($(this).index()).removeClass('highlight');
+			$(this).find('video').get(0).pause();
+		});
+
+
+	//////////////
+	//// AJAX ////
+	//////////////
+
+	$('body').append('<section id="ajax"><div class="loader"></div><nav id="ajax-menu"><a class="close" href="javascript:void(0);">&#215;</a></nav><article id="ajax-content"></article></section>');
+
+	$('a.ajax').click(function(event){
+		event.preventDefault();
+		var url = $(this).attr('href');
+		$('body').addClass('fixed-background');
+		$('#ajax').fadeIn('fast');
+		$('#ajax .loader').show();
+
+		$.ajax({
+			url: url,
+		}).done(function(data){
+			ajaxOpen = true;
+			$('#ajax .loader').hide();
+			$('#ajax-content').hide().html(data).fadeIn();
+
+			// CODE HIGHLIGHT
+			$('#ajax-content pre code').each(function(i, block) {
+				hljs.highlightBlock(block);
+			});
+		});
+	});
+	$('#ajax-menu a.close').click(function(event){
+		event.preventDefault();
+		ajaxClose();
+	});
+
+});
+
+
+//////////////
+//// AJAX ////
+//////////////
+
+function ajaxClose() {
+	$('body').removeClass('fixed-background');
+	$('#ajax').hide();
+	$('#ajax-content').empty();
+	ajaxOpen = false;
+}
+
